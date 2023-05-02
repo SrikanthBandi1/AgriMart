f28f7aa21343180acc75d266f67bdd8ef1ea695e
Sravani Julapalli <sj21378@corp.ojas-it.com>
2023-05-02 17:22:37 +0530

    added customer navbar

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
+import CustomerNavbar from "./CustomerNavbar";
 
 function Layout(props) {
   return (
     <div>
-      <Header />
+      <CustomerNavbar/>
       {props.children}
       <Footer />
     </div>
